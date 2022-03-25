7991e7775ce114e537baeae7f27bbda193536340
mi-hee-k <kim323mh@gmail.com>
2022-03-25 17:23:54 +0900

    add editor and preview layout

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import styles from "./login.module.css";
 
 const Login = ({ authService }) => {
+  const navigate = useNavigate();
+  const goToMaker = (userId) => {
+    navigate("/maker", { state: { id: userId } });
+  };
+
   const onLogin = (e) => {
     authService //
       .login(e.currentTarget.textContent)
-      .then(console.log);
+      .then((data) => goToMaker(data.user.uid));
   };
+
+  useEffect(() => {
+    authService //
+      .onAuthChange((user) => {
+        user && goToMaker(user.uid);
+      });
+  });
   return (
     <section className={styles.login}>
       <Header />
---
+++ src/components/maker/maker.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Header from "../header/header";
+import Footer from "../footer/footer";
+import Editor from "../editor/editor";
+import Preview from "../preview/preview";
+import styles from "./maker.module.css";
+
+const Maker = ({ authService }) => {
+  const navigate = useNavigate();
+  const onLogout = () => {
+    authService.logout();
+  };
+
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      if (!user) {
+        navigate("/");
+      }
+    });
+  });
+  return (
+    <section className={styles.maker}>
+      <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor />
+        <Preview />
+      </div>
+      <Footer />
+    </section>
+  );
+};
+
+export default Maker;
